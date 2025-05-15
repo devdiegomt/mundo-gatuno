db34b7ad4439ac27ba80367fdf22a236ac843c75
devdiegomt <devdiegomt@gmail.com>
2025-05-15 13:17:47 -0500

    feat: footer updated.

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,7 +2,6 @@ export const Footer = () => {
   return (
     <footer className="footer">
       Developed by <a href="https://devdiegomt.vercel.app/">Diego Mayorga</a>.
-      &copy; - Todos los derechos reservados.
     </footer>
   );
 };
