52af3364dfffe6ed12692a80ba23b6d90341d798
devdiegomt <devdiegomt@gmail.com>
2025-05-15 13:22:59 -0500

    feat: footer updated.

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,9 +1,16 @@
+import classes from "./Footer.module.css";
+
 export const Footer = () => {
   return (
     <footer className="footer">
-      Developed by{" "}
+      Developed by
       <b>
-        <a href="https://devdiegomt.vercel.app/"> Diego Mayorga</a>
+        <a
+          className={classes["footer__link"]}
+          href="https://devdiegomt.vercel.app/"
+        >
+          Diego Mayorga
+        </a>
       </b>
       .
     </footer>
